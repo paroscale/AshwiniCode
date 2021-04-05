86d422c8eeb5eb2e1977934d13dd72529243fa7e
Ashwini Patil <ashwinip309@gmail.com>
2021-04-05 10:49:53 +0530

    Basic CFFI code using structure taking char values (writing functions in C  and calling that function in Python and passing structure char values from Python)

--- Python.py
+++ Python.py
@@ -1,8 +1,8 @@
 import myPythonCBinding
-
+myPythonCBinding.lib.call_python_func(a)
 def read():
     print("in python function") 
-    myPythonCBinding.lib.call_python_func(a)
+     
 
 if __name__ == "__main__":
     a = ("abcd".encode('ascii'),"efgh".encode('ascii'))
